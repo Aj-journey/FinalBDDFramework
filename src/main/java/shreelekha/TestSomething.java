45a8ee4c27ce2305e52e987e140ec5c203924167
Shreelekha2242 <jadhav172242@gmail.com>
2025-01-23 00:33:14 +0530

    Commit2--Added print statement

---
+++ src/main/java/shreelekha/TestSomething.java
@@ -0,0 +1,13 @@
+package shreelekha;
+
+public class TestSomething {
+	public static void main(String[] args) {
+		System.out.println("Shreelekha 1");
+		System.out.println("Shreelekha 2");
+		System.out.println("Shreelekha 3");
+		System.out.println("Shreelekha 4");
+		System.out.println("Shreelekha 5");
+		
+	}
+
+}
