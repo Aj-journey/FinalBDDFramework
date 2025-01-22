0527aa9193fe49b2863693233579f992c1a1a102
Shreelekha2242 <jadhav172242@gmail.com>
2025-01-23 01:04:00 +0530

    Commit- shriya new 1

---
+++ src/main/java/PageLayer/TestSomethingShriya.java
@@ -0,0 +1,13 @@
+package PageLayer;
+
+public class TestSomethingShriya {
+	public static void main(String[] args) {
+		System.out.println("Shreelekha 1");
+		System.out.println("Shreelekha 2");
+		System.out.println("Shreelekha 3");
+		System.out.println("Shreelekha 4");
+		System.out.println("Shreelekha 5");
+		
+	}
+
+}
