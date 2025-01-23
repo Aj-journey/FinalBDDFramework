c1afe50f6a0a7c7503f59d21a7221d7b2fb3b81c
kshitij-88 <kshitij06041997@gmail.com>
2025-01-23 12:43:17 +0530

    kshitij sub branch push 2

---
+++ src/main/java/kshitij/Branch2.java
@@ -0,0 +1,12 @@
+package kshitij;
+
+public class Branch2 {
+
+	public static void main(String[] args) {
+	System.out.println("Branch push code 2");
+	System.out.println("Branch push code 2");
+	System.out.println("Branch push code 2");
+
+	}
+
+}
