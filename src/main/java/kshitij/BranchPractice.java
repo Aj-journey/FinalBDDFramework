439ac24ff66e7cd59ab784bfc70ebf7f93caf599
kshitij-88 <kshitij06041997@gmail.com>
2025-01-23 12:32:59 +0530

    kshitij branch push

---
+++ src/main/java/kshitij/BranchPractice.java
@@ -0,0 +1,12 @@
+package kshitij;
+
+public class BranchPractice {
+
+	public static void main(String[] args) {
+	System.out.println("first push in branch");
+	System.out.println("first push in branch");
+	System.out.println("first push in branch");
+
+	}
+
+}
