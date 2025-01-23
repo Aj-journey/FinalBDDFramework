228cbc1ffbcfb7772df5a8494c1f3fee0d53c3ef
123vaishnavi456 <vaishnavichaukhande@gmail.com>
2025-01-23 11:53:17 +0530

    Vaishnavi  Updated Automation TestCase

---
+++ src/main/java/VaishnaviGitHub/VaishnaviGitHub2.java
@@ -0,0 +1,14 @@
+package VaishnaviGitHub;
+
+public class VaishnaviGitHub2 {
+
+	public static void main(String[] args) {
+		System.out.println("Github Demo 2");
+		System.out.println("Github Demo 2");
+		System.out.println("Github Demo 2");
+		System.out.println("Github Demo 2");
+		System.out.println("Github Demo 2");
+
+	}
+
+}
